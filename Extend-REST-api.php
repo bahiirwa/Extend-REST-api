f4fb2a84637b78eb82dce0e4d87ac50ad0bf4e4c
Laurence Bahiirwa <laurence@mediauganda.com>
2018-07-29 08:01:18 +0300

    Update Extend-REST-api.php

--- Extend-REST-api.php
+++ Extend-REST-api.php
@@ -15,9 +15,6 @@
  *
 **/
 
-/* Kudos https://medium.com/@dalenguyen/how-to-get-featured-image-from-wordpress-rest-api-5e023b9896c6 */
-
-
 add_action('rest_api_init', 'register_rest_images' );
 
 function register_rest_images(){
@@ -25,6 +22,42 @@ function register_rest_images(){
         'fimg_url',
         array(
             'get_callback'    => 'get_rest_featured_image',
+            'update_callback' => null,
+            'schema'          => null,
+        )
+    );
+    
+    register_rest_field( array('post'),
+        'featimgs_url',
+        array(
+            'get_callback'    => 'get_rest_attached_images',
+            'update_callback' => null,
+            'schema'          => null,
+        )
+    );
+	
+    register_rest_field( array('post'),
+        'attached_imgs_url',
+        array(
+            'get_callback'    => 'get_rest_attached_images_minus_feat',
+            'update_callback' => null,
+            'schema'          => null,
+        )
+    );
+	
+    register_rest_field( array('post'),
+        'user_display_name',
+        array(
+            'get_callback'    => 'get_rest_user_display_name',
+            'update_callback' => null,
+            'schema'          => null,
+        )
+    );
+	
+    register_rest_field( array('post'),
+        'author_user_description',
+        array(
+            'get_callback'    => 'get_rest_user_description',
             'update_callback' => null,
             'schema'          => null,
         )
@@ -37,4 +70,41 @@ function get_rest_featured_image( $object, $field_name, $request ) {
         return $img[0];
     }
     return false;
+}
+
+function get_rest_attached_images() {
+    $media = get_attached_media('image', get_the_ID());
+    return $media;
+}
+
+//All other attached images minus the featured image.
+function get_rest_attached_images_minus_feat() {
+	$output = "";
+    if( has_post_thumbnail() ): 
+            $output = wp_get_attachment_url( get_post_thumbnail_id( get_the_ID() ) );
+        else:   
+            if($attachment):
+                foreach($attachments as $attachment):
+                    $output = wp_get_attachment_url( $attachment->ID);
+                     
+                endforeach;
+            endif;
+            wp_reset_postdata();
+        endif;
+ 
+    $attachments = get_posts(array(
+        'post_type'=> 'attachment',
+        'posts_per_page' => -1,
+        'post_parent' => get_the_ID()
+        ));
+    return $output;
+}
+
+function get_rest_user_display_name() {
+	return the_author();
+}
+
+
+function get_rest_user_description() {
+	return the_author_description();
 }
